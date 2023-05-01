ba05b50058d42e48e7908498f7e8f24437f9ac27
nilsonsilva28 <odenilsonsilva28@gmail.com>
2023-05-01 17:51:23 -0400

    first commit

---
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Usuario
+
+def home(request):
+    return render(request,'usuarios/home.html')
+
+def usuarios(request):
+    #salvar os dados para tela do banco de dados 
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    #exibir todos os usuarios ja cadastrados em uma nova pagina
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    #retorna os dados para pagina de listagem de usuarios 
+    return render(request,'usuarios/usuarios.html',usuarios)
+    
